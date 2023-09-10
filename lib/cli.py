52bc8aed471cb7202e4cfba81867c21ec5c3dddc
isamu <isamu@to-kyo.to>
2023-09-11 07:27:58 +0900

    add options

--- lib/cli.py
+++ lib/cli.py
@@ -11,17 +11,26 @@ from lib.utils.help import ONELINE_HELP
 def cli():
     parser = argparse.ArgumentParser(description="SlashGPT: LLM Playgroud")
     parser.add_argument("--autotest", action="store_true")
+    parser.add_argument("--agent", default="dispatcher")
+    parser.add_argument("--run")
+
     args = parser.parse_args()
 
+
+    
     with open("./manifests/manifests.json", "r") as f:
         manifests_manager = json.load(f)
 
     dir = manifests_manager["main"]["manifests_dir"]
     config = ChatSlashConfig("./" + dir, llm_models, llm_engine_configs)
     print(ONELINE_HELP)
-    main = SlashGPT(config, manifests_manager, "dispatcher")
+    main = SlashGPT(config, manifests_manager, args.agent)
     if args.autotest:
         main.talk("/autotest")
         main.talk("/bye")
+    if args.run:
+        commands = args.run.split(",")
+        for command in commands:
+            main.talk(command)
 
     main.start()
