6c068d7db561784b1e83415804cb3afece845428
isamu <isamu@to-kyo.to>
2023-09-20 07:56:39 +0900

    set_engine

--- src/slashgpt/llms/model.py
+++ src/slashgpt/llms/model.py
@@ -8,7 +8,7 @@ from slashgpt.manifest import Manifest
 class LlmModel:
     def __init__(self, llm_model_data: dict):
         self.llm_model_data = llm_model_data
-        self.set_engine()
+        self.engine = LLMEngineFactory.factory(self.engine_name(), self)
 
     def get(self, key: str):
         return self.llm_model_data.get(key)
@@ -33,9 +33,6 @@ class LlmModel:
 
         # TODO default replicate model
         return "a16z-infra/llama7b-v2-chat:a845a72bb3fa3ae298143d13efa8873a2987dbf3d49c293513cd8abf4b845a83"
-
-    def set_engine(self):
-        self.engine = LLMEngineFactory.factory(self.engine_name(), self)
 
     def generate_response(self, messages: List[dict], manifest: Manifest, verbose: bool):
         return self.engine.chat_completion(messages, manifest, verbose)
